212057072a3eed0d882afb236d7057bc9ed428ce
Tom Ray <tomwray13@gmail.com>
2022-05-31 21:50:21 +0100

    Test prisma service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './services/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getHello(): Promise<any> {
+    return await this.prisma.tweets.findMany();
   }
 }
